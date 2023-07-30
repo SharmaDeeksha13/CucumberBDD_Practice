eecc53ad209a46dab7dbff32bc89984b718bdbe5
deeksha.sharma <deeksha13aug@gmail.com>
2023-07-30 17:52:13 -0400

    added surefire

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -6,7 +6,8 @@ public class Demo {
 
     @BeforeTest
     public void execute1() {
-        System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\chromedriver_win32\\chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver,chrome.driver","D://Softwares//chromedriver_win32//chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://phoenixnap.com/kb/install-maven-windows");
 
