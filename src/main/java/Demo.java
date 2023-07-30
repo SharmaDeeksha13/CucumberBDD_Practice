07b5bd24be87478b250f5425f3e976ff7ec7d1cf
deeksha.sharma <deeksha13aug@gmail.com>
2023-07-30 17:49:06 -0400

    Commit

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,23 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class Demo {
 
-  @BeforeTest
-    public void execute1()
-  {
-      System.out.println("Hello Canada");
+    @BeforeTest
+    public void execute1() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://phoenixnap.com/kb/install-maven-windows");
+
+        System.out.println("Hello Canada");
+        System.out.println("Hello USA");
+        System.out.println("Hello USA");
+        System.out.println("Hello USA");
+        System.out.println("Hello USA");
+        System.out.println("Hello USA");
+        System.out.println("Hello USA");
+        System.out.println("Hello USA");
+        System.out.println("Hello India");
+        System.out.println("Hello India changes done in Deeksha Branch");
 
 
-      System.out.println("Hello USA");
-      System.out.println("Hello USA");
-      System.out.println("Hello USA");
-      System.out.println("Hello USA");
-      System.out.println("Hello USA");
-      System.out.println("Hello USA");
-      System.out.println("Hello USA");
-      System.out.println("Hello India");
-      System.out.println("Hello India changes done in Deeksha Branch");
-
-
-  }
+    }
 }
